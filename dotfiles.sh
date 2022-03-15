bc1e858fd4d30f286c41bf3a0c1d2ac0bc456e4d
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2022-03-14 23:09:32 -0300

    fix: adjust main dotfiles file
    Remove old code
    Import modules
    Create profiles and fonts group functions

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,186 +1,47 @@
 #!/usr/bin/env bash
 
-git-profile () {
+# Functions
+source ./utils/notifyme.sh
 
-    warning "Cloning git config gist..."
+# Profiles
+source ./modules/profiles/git.sh
+source ./modules/profiles/ssh.sh
+source ./modules/profiles/terminal.sh
+source ./modules/profiles/vim.sh
+source ./modules/profiles/zsh.sh
 
-    local clone=$(git clone -q https://gist.github.com/6e11b99a007e660656e147e88fde4bc0 /tmp/gists/git-config)
+# Fonts
+source ./modules/fonts/firacode.sh
+source ./modules/fonts/powerline.sh
+source ./modules/fonts/roboto.sh
 
-    if [[ $clone -eq 0 ]]; then
+profiles () {
 
-        success "Gist successfully cloned"
+    git-config
 
-        if [[ -e "~/.gitconfig" ]]; then
+    ssg-config
 
-            warning "File .git-config already exists"
+    terminal-config
 
-            warning "Replacing content..."
+    vim-config
 
-            mv /tmp/gists/git-config ~/.gitconfig
-
-            success
-
-        else
-
-            warning "File .gitconfig doesn't exists"
-
-            warning "Creating file..."
-
-            cat /tmp/gists/git-config > ~/.gitconfig
-
-            success
-
-        fi
-
-    else
-
-        error
-
-    fi
-
-    warning "Removing /tmp/gists/git-config"
-
-    rm -rf "/tmp/gists/git-config"
-
-    if [[ ! -d "/tmp/gists/git-config" ]]; then
-
-        success "Files successfully removed"
-
-    else
-
-        error
-
-    fi
-
-    echo
+    zsh-config
 
 }
 
+fonts () {
 
+    powerline
 
-copy-profile () {
+    firacode
 
-    local hasPreferences=$(cat ~/.zshrc | grep -c "source ~/dotfiles/zshrc")
-
-    if [[ $hasPreferences -eq 0 ]]; then
-
-        warning "Appending preferences..."
-
-        echo "source ~/dotfiles/zshrc" >> ~/.zshrc
-
-        hasPreferences=$(cat ~/.zshrc | grep -c "source ~/dotfiles/zshrc")
-
-        [[ $hasPreferences -eq 1 ]] && success "Preferences successfully applied"
-
-    else
-
-        warning "Profile already applied"
-
-    fi
-
-    missing-symbols
-
-    echo
-
-}
-
-
-
-terminal-profile () {
-
-    warning "Cloning terminal profile gist..."
-
-    local clone=$(git clone -q https://gist.github.com/828f21060561ac67fa6d526f02c027f4.git /tmp/gists/terminal_profile)
-
-    if [[ $clone -eq 0 ]]; then
-
-        success "Gist successfully cloned"
-
-        warning "Copying terminal profile"
-
-        cat /tmp/gists/terminal_profile/terminal_profile.dconf | dconf load /org/gnome/terminal/legacy/profiles:/
-
-        success
-
-    else
-
-        error
-
-    fi
-
-    warning "Removing /tmp/gists/terminal_profile"
-
-    rm -rf "/tmp/gists/terminal_profile"
-
-    if [[ ! -d "/tmp/gists/terminal_profile" ]]; then
-
-        success "Files successfully removed"
-
-    else
-
-        error
-
-    fi
-
-    echo
-
-}
-
-ssh-config () {
-
-    warning "Cloning SSH Config gist..."
-
-    local clone=$(git clone -q https://gist.github.com/fd6c7eae2e2476390b44d4e83fc50e64.git /tmp/gists/ssh-config)
-
-    if [[ $clone -eq 0 ]]; then
-
-        success "Gist successfully cloned"
-
-        warning "Copying terminal profile"
-
-        cat /tmp/gists/ssh-config/config >> $HOME/.ssh/config
-
-        success
-
-    else
-
-        error
-
-    fi
-
-    warning "Removing /tmp/gists/ssh-config"
-
-    rm -rf "/tmp/gists/ssh-config"
-
-    if [[ ! -d "/tmp/gists/ssh-config" ]]; then
-
-        success "Files successfully removed"
-
-    else
-
-        error
-
-    fi
-
-    echo
+    roboto
 
 }
 
 main () {
 
-    echo "Starting..."
-
-    git-profile
-
-    copy-profile
-
-    roboto-fonts
-
-    terminal-profile
-
-    ssh-config
-
-    echo "Done!"
+    profiles
 
 }
 
